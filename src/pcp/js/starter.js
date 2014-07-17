8a60f81c1f83d8cafc535bd7b993fe32a5865dac
Alexander Milchev <amilchev@asteasolutions.com>
2014-07-17 16:53:02 +0300

    GPII-543: Make pcp render according to JSON preferences (for visualAlternatives group only for now).

--- src/pcp/js/starter.js
+++ src/pcp/js/starter.js
@@ -8,16 +8,57 @@
         }
     });
 
-    gpii.pcp.renderPCP = function (whatToRender) {
+    gpii.pcp.renderPCP = function (preferences) {
+        var visualAlternativesLevels = {0: ["visualAlternatives"], 1: ["visualAlternatives", "speakText"], 2: ["visualAlternatives", "speakText", "visualAlternativesMoreLess"]};
+
+        var levels = [["speakText", "screenReaderBrailleOutput"],
+                      ["wordsSpokenPerMinute", "volume"],
+                      ["voicePitch", "screenReaderLanguage", "punctuationVerbosity", "announceCapitals", "speakTutorialMessages", "keyEcho", "wordEcho", "textHighlighting", "screenReaderFollows"]
+                     ];
+
+        var levelOfAdjuster = function (adjuster) {
+            for (i = 0; i < levels.length; i++) {
+                if ($.inArray(adjuster, levels[i]) > -1) {
+                    return i;
+                };
+            };
+            return -1;
+        };
+
+        var deepestLevel = function (arrayOfAdjusters) {
+            return Math.max.apply(null, arrayOfAdjusters.map(levelOfAdjuster));
+        };
+
+        // for visualAlternatives only for now
+        var determineAdjusterGradeNames = function (arrayOfModelAdjusters) {
+            var commonModelPartLength = 19;
+            var arrayOfAdjusters = fluid.transform(arrayOfModelAdjusters, function (adjuster) {
+                return adjuster.substr(commonModelPartLength);
+            });
+
+            var additionals = visualAlternativesLevels[deepestLevel(arrayOfAdjusters)];
+            for (i = 0; i < additionals.length; i++) {
+                if ($.inArray(additionals[i], arrayOfAdjusters) < 0) {
+                    arrayOfAdjusters.push(additionals[i]);
+                };
+            };
+
+            var arrayOfSchemaAdjusters = fluid.transform(arrayOfAdjusters, function (adjuster) {
+                return "gpii.pcp.auxiliarySchema." + adjuster;
+            });
+
+            return arrayOfSchemaAdjusters;
+        };
+
         var compulsory = ["gpii.pcp.auxiliarySchema.mergePolicy", "gpii.pcp.progressiveEnhancement", "gpii.pcp.auxiliarySchema.common"];
 
-        var components = fluid.transform(whatToRender, function (adjuster) {
-            return "gpii.pcp.auxiliarySchema." + adjuster;
-        });
+        var modelToRender = fluid.model.transform(preferences, gpii.prefs.commonTermsInverseTransformationRules);
+        var modelKeys = Object.keys(modelToRender);
+        var finalAdjusterGradeNames = determineAdjusterGradeNames(modelKeys);
 
         fluid.prefs.create("#gpiic-pcp", {
             build: {
-                gradeNames: compulsory.concat(components),
+                gradeNames: compulsory.concat(finalAdjusterGradeNames),
                 primarySchema: gpii.primarySchema
             },
             prefsEditor: {
